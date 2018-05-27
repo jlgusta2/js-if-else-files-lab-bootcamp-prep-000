e6f209f70a515b7a5d2a72be269f9358295dea83
jennifer gustavson <jengustavson@gmail.com>
2018-05-27 16:12:21 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,7 +1,7 @@
 
 var index = require("./index.js")
 
-if (index.name === "Susan") {
+if (index.name === "Joe") {
     console.log("The name is correct")
 } else {
     console.log("Expected: Susan, Received: "+index.name)
@@ -9,7 +9,7 @@ if (index.name === "Susan") {
 
 
 
-if (index.height === 70) {
+if (index.height === 74) {
     console.log("The height is correct")
 } else {
     console.log("Expected: 70, Received: "+index.height)
